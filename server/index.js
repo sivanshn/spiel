e1833fe7570663050bf347a41edad979883cbe85
Saifan <saifan.shino@uni-oldenburg.de>
2026-05-15 21:36:38 +0200

    Fix: Use Regex object for Express 5 catch-all

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.get('/api/health', (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('/:any*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
