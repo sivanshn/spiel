03556493dd81b91a0ba4e6b4c8709f70d817c151
Saifan <saifan.shino@uni-oldenburg.de>
2026-05-15 21:34:59 +0200

    Fix: Use named parameter for Express 5 catch-all

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.get('/api/health', (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('(.*)', (req, res) => {
+app.get('/:any*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
